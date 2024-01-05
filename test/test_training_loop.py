6fa5492825f0aa0816370e2a9477dfdb41b1a1a5
MathildeDupouy <82039222+MathildeDupouy@users.noreply.github.com>
2024-01-05 12:23:07 +0100

    add default save_dir for unit test

--- test/test_training_loop.py
+++ test/test_training_loop.py
@@ -1,7 +1,9 @@
 import pytest
 from gyraudio.audio_separation.train import launch_training
+from gyraudio.default_locations import EXPERIMENT_STORAGE_ROOT
 
 
 @pytest.mark.parametrize("device", ["cuda", "cpu"])
 def test_training_loop(device):
-    launch_training(0, wandb_flag=False, device=device)
+    launch_training(0, wandb_flag=False, device=device, save_dir=EXPERIMENT_STORAGE_ROOT)
+    
