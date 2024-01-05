1d9fe70904496bf95f43920cb9aa3e1b338b3510
Balthazar Neveu <balthazarneveu@gmail.com>
2024-01-05 14:13:44 +0100

    #8 remote training on kaggle

---
+++ scripts/audio_separation_train_remote.py
@@ -0,0 +1,98 @@
+import kaggle
+from pathlib import Path
+import json
+from __kaggle_login import kaggle_users
+import argparse
+import sys
+import subprocess
+from gyraudio import root_dir as ROOT_DIR
+
+
+def get_git_branch_name():
+    try:
+        branch_name = subprocess.check_output(["git", "branch", "--show-current"]).strip().decode()
+        return branch_name
+    except subprocess.CalledProcessError:
+        return "Error: Could not determine the Git branch name."
+
+
+def prepare_notebook(
+    output_nb_path: Path,
+    exp: int,
+    branch: str,
+    git_user: str = "balthazarneveu",
+    git_repo: str = "gyraudio",
+    template_nb_path: Path = Path(__file__).parent/"remote_training_template.ipynb"
+):
+    expressions = [
+        ("exp", f"{exp}"),
+        ("branch", f"\'{branch}\'"),
+        ("git_user", f"\'{git_user}\'"),
+        ("git_repo", f"\'{git_repo}\'")
+    ]
+    with open(template_nb_path) as f:
+        template_nb = f.readlines()
+        for line_idx, li in enumerate(template_nb):
+            for expr, expr_replace in expressions:
+                if f"!!!{expr}!!!" in li:
+                    template_nb[line_idx] = template_nb[line_idx].replace(f"!!!{expr}!!!", expr_replace)
+        template_nb = "".join(template_nb)
+    with open(output_nb_path, "w") as w:
+        w.write(template_nb)
+
+
+def main(argv):
+    nb_id = "train-audio-separation"
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-u", "--user", type=str, help="Kaggle user", choices=list(kaggle_users.keys()))
+    parser.add_argument("-e", "--exp", type=int, required=True, help="Experiment id")
+    parser.add_argument("--cpu", action="store_true", help="Force CPU")
+    parser.add_argument("--branch", type=str, help="Git branch name", default=get_git_branch_name())
+    parser.add_argument("-p", "--push", action="store_true", help="Push")
+    parser.add_argument("-d", "--download", action="store_true", help="Download results")
+    args = parser.parse_args(argv)
+    exp = args.exp
+    kaggle_user = kaggle_users[args.user]
+    uname_kaggle = kaggle_user["username"]
+    kaggle.api._load_config(kaggle_user)
+    if args.download:
+        tmp_dir = ROOT_DIR/f"__tmp_{exp:04d}"
+        tmp_dir.mkdir(exist_ok=True, parents=True)
+        kaggle.api.kernels_output_cli(f"{kaggle_user['username']}/{nb_id}", path=str(tmp_dir))
+        subprocess.run(["tar", "-xzf", tmp_dir/"output.tgz", "__output"])
+        import shutil
+        shutil.rmtree(tmp_dir, ignore_errors=True)
+        return
+    kernel_root = ROOT_DIR/f"__nb_{uname_kaggle}"
+    kernel_root.mkdir(exist_ok=True, parents=True)
+
+    kernel_path = kernel_root/f"{exp:04d}"
+    kernel_path.mkdir(exist_ok=True, parents=True)
+    branch = args.branch
+    config = {
+        "id": str(Path(f"{kaggle_user['username']}")/nb_id),
+        "title": nb_id.lower(),
+        "code_file": f"{nb_id}.ipynb",
+        "language": "python",
+        "kernel_type": "notebook",
+        "is_private": "true",
+        "enable_gpu": "true" if not args.cpu else "false",
+        "enable_tpu": "false",
+        "enable_internet": "true",
+        "dataset_sources": ["balthazarneveu/audio-separation-dataset"],
+        "competition_sources": [],
+        "kernel_sources": [],
+        "model_sources": []
+    }
+    prepare_notebook((kernel_path/nb_id).with_suffix(".ipynb"), exp, branch)
+    assert (kernel_path/nb_id).with_suffix(".ipynb").exists()
+    with open(kernel_path/"kernel-metadata.json", "w") as f:
+        json.dump(config, f, indent=4)
+
+    if args.push:
+        kaggle.api.kernels_push_cli(str(kernel_path))
+
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
--- src/gyraudio/audio_separation/readme.md
+++ src/gyraudio/audio_separation/readme.md
@@ -1,26 +1,13 @@
-# Pytest
+# Audio separation
+## :bug: Pytest
+
 ```bash
 pytest-3 test
 ```
-### Dataloader
-[Mixed dataloader](/src/gyraudio/audio_separation/data/mixed.py)
-- Loads mixed, signal, noise.
-- Audio tensors of sizes `[N, 1, T]`, `T=8000` by default.
-- Possibility to filter by SNR.
-Provided data:
-```python
-SNR: 0 - TRAIN SIZE 1001 TEST SIZE 395
-SNR: 1 - TRAIN SIZE 506 TEST SIZE 209
-SNR: 2 - TRAIN SIZE 485 TEST SIZE 171
-SNR: 3 - TRAIN SIZE 509 TEST SIZE 206
-SNR: 4 - TRAIN SIZE 520 TEST SIZE 200
-SNR: -1 - TRAIN SIZE 506 TEST SIZE 201
-SNR: -2 - TRAIN SIZE 507 TEST SIZE 205
-SNR: -4 - TRAIN SIZE 516 TEST SIZE 206
-SNR: -3 - TRAIN SIZE 450 TEST SIZE 207
-```
+
 
 ## :chart_with_downwards_trend: Training
+
 :id: Keep track of experiments by an integer id. 
 
 Each experiment is defined by:
@@ -28,6 +15,36 @@ Each experiment is defined by:
 - Model (architecture, sizes)
 - Optimizer configuration (hyperparameters)
 
+### Defining experiments
+[Code to define new experiments](/src/gyraudio/audio_separation/experiment_tracking/experiments_definition.py)
+### Remote training
+
+- :unlock: Create a [scripts/__kaggle_login.py](/scripts/__kaggle_login.py) file locally.
+```python
+kaggle_users = {
+    "user1": {
+        "username": "user1_kaggle_name",
+        "key": "user1_kaggle_key"
+    },
+    "user2": {
+        "username": "user2_kaggle_name",
+        "key": "user2_kaggle_key"
+    },
+}
+```
+- :key: Allow Kaggle secrets to access wandb:
+  - `wandb_api_key`: weights and biases API key 
+  - :phone: a verified kaggle account is required
+
+Run `python scripts/remote_training.py -u user1 -e X`
+This will create a dedicated folder for training a specific experiment with a dedicated notebook.
+- use **`--cpu`** to setup at the begining (avoid using GPU)
+- use **`-p`** (`--push`) will upload/push the notebook and run it.
+- use **`-d`** (`--download`) to download the training results and save it to disk)
+Please note that the first time, you'll need to manually edit the notebook under kaggle web page to allow secrets.
+
+
+### Local training
 `python scripts/audio_separation_train.py -e 1`
 
 ### :bar_chart: [Experiment tracking](https://wandb.ai/balthazarneveu/audio-sep)
@@ -48,3 +65,21 @@ python scripts/audio_separation_interactive.py -i __data_source_separation/sourc
 
 - :arrow_backward: :arrow_forward: = next audio `left` / `right`
 - :arrow_double_down: :arrow_double_up: = next model `page up` / `page down` (*compare models*)
+
+### Dataloader
+[Mixed dataloader](/src/gyraudio/audio_separation/data/mixed.py)
+- Loads mixed, signal, noise.
+- Audio tensors of sizes `[N, 1, T]`, `T=8000` by default.
+- Possibility to filter by SNR.
+Provided data:
+```python
+SNR: 0 - TRAIN SIZE 1001 TEST SIZE 395
+SNR: 1 - TRAIN SIZE 506 TEST SIZE 209
+SNR: 2 - TRAIN SIZE 485 TEST SIZE 171
+SNR: 3 - TRAIN SIZE 509 TEST SIZE 206
+SNR: 4 - TRAIN SIZE 520 TEST SIZE 200
+SNR: -1 - TRAIN SIZE 506 TEST SIZE 201
+SNR: -2 - TRAIN SIZE 507 TEST SIZE 205
+SNR: -4 - TRAIN SIZE 516 TEST SIZE 206
+SNR: -3 - TRAIN SIZE 450 TEST SIZE 207
+```
