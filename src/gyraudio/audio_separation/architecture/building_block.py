78c440f19c8bbff9149eafaac49215d4d2dfb7dd
Balthazar Neveu <balthazarneveu@gmail.com>
2024-01-06 15:25:54 +0100

    #10 add filter bank (for audio embedding)

--- src/gyraudio/audio_separation/architecture/building_block.py
+++ src/gyraudio/audio_separation/architecture/building_block.py
@@ -1,4 +1,25 @@
 import torch
+from typing import List
+
+
+class FilterBank(torch.nn.Module):
+    """Convolution filter bank (linear)
+    Serves as an embedding for the audio signal
+    """
+
+    def __init__(self, ch_in: int, out_dim=16, k_size=5, dilation_list: List[int] = [1, 2, 4, 8]):
+        super().__init__()
+        self.out_dim = out_dim
+        self.source_modality_conv = torch.nn.ModuleList()
+        for dilation in dilation_list:
+            self.source_modality_conv.append(
+                torch.nn.Conv1d(ch_in, out_dim//len(dilation_list), k_size, dilation=dilation, padding=(dilation*(k_size//2)))
+            )
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        out = torch.cat([conv(x) for conv in self.source_modality_conv], axis=1)
+        assert out.shape[1] == self.out_dim
+        return out
 
 
 class ResConvolution(torch.nn.Module):
@@ -20,3 +41,11 @@ class ResConvolution(torch.nn.Module):
         x += x_in
         x = self.non_linearity(x)
         return x
+
+
+if __name__ == "__main__":
+    model = FilterBank(1, 16)
+    inp = torch.rand(2, 1, 2048)
+    out = model(inp)
+    print(model)
+    print(out[0].shape)
