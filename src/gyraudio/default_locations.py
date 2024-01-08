0d20ab6c7736a0490e14a09a7b00fec1d262fb66
Balthazar Neveu <balthazarneveu@gmail.com>
2024-01-08 14:39:42 +0100

    #4 add audio samples

--- src/gyraudio/default_locations.py
+++ src/gyraudio/default_locations.py
@@ -2,3 +2,4 @@ from gyraudio import root_dir
 RAW_AUDIO_ROOT = root_dir/"__data_source_separation"/"voice_origin"
 MIXED_AUDIO_ROOT = root_dir/"__data_source_separation"/"source_separation"
 EXPERIMENT_STORAGE_ROOT = root_dir/"__output"
+SAMPLE_ROOT = root_dir/"audio_samples"
