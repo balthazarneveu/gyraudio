72553fb524986602f3cc856754c81f87e12474b8
Balthazar Neveu <balthazarneveu@gmail.com>
2024-01-10 17:14:39 +0100

    #13 allow AWGN and scaling RESCALE augementations

--- src/gyraudio/audio_separation/data/mixed.py
+++ src/gyraudio/audio_separation/data/mixed.py
@@ -1,5 +1,5 @@
 from gyraudio.audio_separation.data.dataset import AudioDataset
-
+from gyraudio.audio_separation.properties import AUG_AWGN, AUG_RESCALE
 import logging
 import torch
 import torchaudio
@@ -33,6 +33,21 @@ class MixedAudioDataset(AudioDataset):
         clean_audio_signal, sampling_rate = torchaudio.load(str(signal_path))
         noise_audio_signal, sampling_rate = torchaudio.load(str(noise_path))
         self.sampling_rate = sampling_rate
+        if AUG_RESCALE in self.augmentation_config:
+            noise_std = self.augmentation_config[AUG_RESCALE]
+            current_amplitude = 0.5 + 1.5*torch.rand(1, device=mixed_audio_signal.device)
+            # logging.debug(current_amplitude)
+            mixed_audio_signal *= current_amplitude
+            noise_audio_signal *= current_amplitude
+            clean_audio_signal *= current_amplitude
+        if AUG_AWGN in self.augmentation_config:
+            # noise_std = self.augmentation_config[AUG_AWGN]["noise_std"]
+            noise_std = 0.01
+            current_noise_std = torch.randn(1) * noise_std
+            # logging.debug(current_noise_std)
+            extra_awgn = torch.randn(mixed_audio_signal.shape, device=mixed_audio_signal.device) * current_noise_std
+            clean_audio_signal = clean_audio_signal+extra_awgn
+            # Open question: should we add noise to the noise signal aswell?
         if self.debug:
             logging.debug(f"{mixed_audio_signal.shape}")
             logging.debug(f"{clean_audio_signal.shape}")
