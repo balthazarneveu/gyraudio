dcf4b8341ce46f993b106d285722aec0338d16a9
Balthazar Neveu <balthazarneveu@gmail.com>
2024-01-10 23:47:09 +0100

    #6 add ping pong mechanism to save audio files (avoid using a mutex /thread lock) - play and loop audio

--- src/gyraudio/audio_separation/visualization/audio_player.py
+++ src/gyraudio/audio_separation/visualization/audio_player.py
@@ -1,9 +1,11 @@
 from gyraudio.audio_separation.properties import CLEAN, NOISY, MIXED, PREDICTED
 from pathlib import Path
 from gyraudio.io.audio import save_audio_tensor
+from gyraudio import root_dir
 from interactive_pipe import Control
 from interactive_pipe import interactive
 import logging
+import torch
 
 HERE = Path(__file__).parent
 MUTE = "mute"
@@ -17,11 +19,14 @@ LOGOS = {
 ICONS = [it for key, it in LOGOS.items()]
 KEYS = [key for key, it in LOGOS.items()]
 
+ping_pong_index = 0
+
 
 @interactive(
     volume=(100, [0, 1000], "volume"),
     player=Control(MUTE, KEYS, icons=ICONS))
 def audio_player(sig, mixed, pred, global_params={}, volume=100, player=MUTE):
+    sampling_rate = global_params.get("sampling_rate", 8000)
     try:
         if player == MUTE:
             global_params["__stop"]()
@@ -34,9 +39,24 @@ def audio_player(sig, mixed, pred, global_params={}, volume=100, player=MUTE):
                 audio_track = mixed
             elif player == PREDICTED:
                 audio_track = pred
-            audio_track_path = "_tmp.wav"
-            save_audio_tensor(audio_track_path, volume/100.*audio_track,
-                              sampling_rate=global_params.get("sampling_rate", 8000))
+            ping_pong_path = root_dir/"__ping_pong"
+            ping_pong_path.mkdir(exist_ok=True)
+            global ping_pong_index
+            audio_track_path = ping_pong_path/f"_tmp_{ping_pong_index}.wav"
+            ping_pong_index = (ping_pong_index + 1) % 10
+            if global_params.get("trim", False):
+                start, end = global_params["trim"]["start"], global_params["trim"]["end"]
+                remainder = (end-start) % 8
+                audio_trim = audio_track[..., start:end-remainder]
+                repeat_factor = int(sampling_rate*4./(end-start))
+                logging.debug(f"{repeat_factor}")
+                repeat_factor = max(1, repeat_factor)
+                audio_trim = audio_trim.repeat(1, repeat_factor)
+                logging.debug(f"{audio_trim.shape}")
+            else:
+                audio_trim = audio_track
+            save_audio_tensor(audio_track_path, volume/100.*audio_trim,
+                              sampling_rate=global_params.get("sampling_rate", sampling_rate))
             global_params["__set_audio"](audio_track_path)
             global_params["__play"]()
     except Exception as exc:
