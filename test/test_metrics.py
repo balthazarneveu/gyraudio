848dd7d6387503d89d43f56eb4118aa3f0575ec0
Balthazar Neveu <balthazarneveu@gmail.com>
2024-01-13 21:33:59 +0100

    #18 add metrics

---
+++ test/test_metrics.py
@@ -0,0 +1,24 @@
+from gyraudio.audio_separation.metrics import Metrics
+from gyraudio.audio_separation.properties import SIGNAL, NOISE
+import torch
+
+
+def test_metrics(device="cpu"):
+    metric = Metrics("check")
+    batch_size = 49096*4
+    gt_1 = torch.zeros(batch_size, 1, 512, device=device, requires_grad=True)
+    gt_2 = torch.zeros(batch_size, 1, 256, device=device)
+    for epoch in range(4):
+        for step in range(10):
+            # Prediction
+            pred_1 = step*torch.randn(*gt_1.shape, device=device)
+            pred_2 = epoch*torch.ones(*gt_2.shape, device=device)
+            metric.update(pred_1, gt_1, SIGNAL)
+            metric.update(pred_2, gt_2, NOISE)
+            loss = metric.finish_step()
+            # Backprop/update weights etc...
+            loss.backward()
+            print(f"epoch {epoch} | step {step} : {metric.metrics}")
+        metric.finish_epoch()
+        print(f"epoch {epoch} >>>>>>>> : {metric.total_metric}")
+        metric.reset_epoch()
