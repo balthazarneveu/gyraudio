996d0bc0af9212ab93063568cb36d6cd1f8ec953
MathildeDupouy <82039222+MathildeDupouy@users.noreply.github.com>
2024-01-13 15:12:02 +0100

    factorize augmentation

--- src/gyraudio/audio_separation/data/remixed.py
+++ src/gyraudio/audio_separation/data/remixed.py
@@ -39,20 +39,7 @@ class RemixedAudioDataset(AudioDataset):
         power_target_sqrt = 16.
         mixed_audio_signal = mixed_audio_signal * power_target_sqrt / torch.norm(mixed_audio_signal)
         self.sampling_rate = sampling_rate
-        if AUG_RESCALE in self.augmentation_config:
-            current_amplitude = 0.5 + 1.5*torch.rand(1, device=mixed_audio_signal.device)
-            # logging.debug(current_amplitude)
-            mixed_audio_signal *= current_amplitude
-            noise_audio_signal *= current_amplitude
-            clean_audio_signal *= current_amplitude
-        if AUG_AWGN in self.augmentation_config:
-            # noise_std = self.augmentation_config[AUG_AWGN]["noise_std"]
-            noise_std = 0.01
-            current_noise_std = torch.randn(1) * noise_std
-            # logging.debug(current_noise_std)
-            extra_awgn = torch.randn(mixed_audio_signal.shape, device=mixed_audio_signal.device) * current_noise_std
-            mixed_audio_signal = mixed_audio_signal+extra_awgn
-            # Open question: should we add noise to the noise signal aswell?
+        mixed_audio_signal, clean_audio_signal, noise_audio_signal = self.augment_data(mixed_audio_signal, clean_audio_signal, noise_audio_signal)
         if self.debug:
             logging.debug(f"{mixed_audio_signal.shape}")
             logging.debug(f"{clean_audio_signal.shape}")
