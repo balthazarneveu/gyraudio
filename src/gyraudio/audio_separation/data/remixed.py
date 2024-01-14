835958b8ce19719b6a974a783ce7b874ef816254
MathildeDupouy <82039222+MathildeDupouy@users.noreply.github.com>
2024-01-14 10:59:04 +0100

    remixed fixed and rnd + unit test remixed

--- src/gyraudio/audio_separation/data/remixed.py
+++ src/gyraudio/audio_separation/data/remixed.py
@@ -4,13 +4,12 @@ import logging
 from torch import Tensor
 import torch
 import torchaudio
-from random import randint
-from gyraudio.audio_separation.properties import (
-    AUG_AWGN, AUG_RESCALE
-)
 
 
 class RemixedAudioDataset(AudioDataset):
+    def generate_snr_list(self) :
+        self.snr_list = None
+
     def load_data(self):
         self.folder_list = sorted(list(self.data_path.iterdir()))
         self.file_list = [
@@ -19,25 +18,32 @@ class RemixedAudioDataset(AudioDataset):
                 folder/"noise.wav"
             ] for folder in self.folder_list
         ]
-        if self.debug:
-            print("Filtered", len(self.file_list), self.snr_filter)
         self.sampling_rate = None
+        self.min_snr, self.max_snr = -4, 4
+        self.generate_snr_list()
+        if self.debug:
+            print("Not filtered", len(self.file_list), self.snr_filter)
+            print(self.snr_list)
+
+
+    def get_idx_noise(self, idx) :
+        raise NotImplementedError("get_idx_noise method must be implemented")
+
+    def get_snr(self, idx) :
+        raise NotImplementedError("get_snr method must be implemented")
 
     def __getitem__(self, idx: int) -> Tuple[Tensor, Tensor, Tensor]:
         signal_path = self.file_list[idx][0]
-        idx_noise = randint(0, len(self.file_list)-1)
+        idx_noise = self.get_idx_noise(idx)
         noise_path = self.file_list[idx_noise][1]
 
         assert signal_path.exists()
         assert noise_path.exists()
         clean_audio_signal, sampling_rate = torchaudio.load(str(signal_path))
         noise_audio_signal, sampling_rate = torchaudio.load(str(noise_path))
-        min_snr, max_snr = -4, 4
-        snr = min_snr + (max_snr-min_snr)*torch.rand(1)
+        snr = self.get_snr(idx)
         alpha = 10 ** (-snr / 20) * torch.norm(clean_audio_signal) / torch.norm(noise_audio_signal)
         mixed_audio_signal = clean_audio_signal + alpha*noise_audio_signal
-        power_target_sqrt = 16.
-        mixed_audio_signal = mixed_audio_signal * power_target_sqrt / torch.norm(mixed_audio_signal)
         self.sampling_rate = sampling_rate
         mixed_audio_signal, clean_audio_signal, noise_audio_signal = self.augment_data(mixed_audio_signal, clean_audio_signal, noise_audio_signal)
         if self.debug:
---
+++ src/gyraudio/audio_separation/data/remixed_rnd.py
@@ -0,0 +1,11 @@
+from gyraudio.audio_separation.data.remixed import RemixedAudioDataset
+from torch import rand, randint
+
+class RemixedRandomAudioDataset(RemixedAudioDataset):
+    def get_idx_noise(self, idx) :
+        return randint(0, len(self.file_list)-1, (1,))
+
+    def get_snr(self, idx) :
+        if self.snr_filter is None :
+            return self.min_snr + (self.max_snr - self.min_snr)*rand(1)
+        return self.snr_filter[randint(0, len(self.snr_filter), (1,))]
