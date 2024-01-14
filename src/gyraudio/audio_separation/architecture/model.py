181806c3a2b09e7c183ce0847d519f1f6e62d945
Balthazar Neveu <balthazarneveu@gmail.com>
2024-01-14 10:59:04 +0100

    #18 estimate receptive field

--- src/gyraudio/audio_separation/architecture/model.py
+++ src/gyraudio/audio_separation/architecture/model.py
@@ -6,4 +6,23 @@ class SeparationModel(torch.nn.Module):
         super().__init__(*args, **kwargs)
 
     def count_parameters(self) -> int:
+        """Count the total number of parameters of the model
+
+        Returns:
+            int: total amount of parameters
+        """
         return sum(p.numel() for p in self.parameters() if p.requires_grad)
+
+    def receptive_field(self) -> int:
+        """Compute the receptive field of the model
+
+        Returns:
+            int: receptive field
+        """
+        input_tensor = torch.rand(1, 1, 4096, requires_grad=True)
+        out, out_noise = self.forward(input_tensor)
+        grad = torch.zeros_like(out)
+        grad[..., out.shape[-1]//2] = torch.nan  # set NaN gradient at the middle of the output
+        out.backward(gradient=grad)
+        self.zero_grad()  # reset to avoid future problems
+        return torch.sum(input_tensor.grad.isnan())  # Count NaN in the input
