b9841a6690c521b6b67960246852713b68df6239
MathildeDupouy <82039222+MathildeDupouy@users.noreply.github.com>
2024-01-14 10:59:04 +0100

    remixed fixed and rnd + unit test remixed

--- test/test_dataloader.py
+++ test/test_dataloader.py
@@ -1,7 +1,8 @@
 from gyraudio.audio_separation.properties import TRAIN, TEST, AUG_TRIM
-from gyraudio.audio_separation.data import get_dataloader, get_config_dataloader, SingleAudioDataset, MixedAudioDataset
+from gyraudio.audio_separation.data import get_dataloader, get_config_dataloader, SingleAudioDataset, MixedAudioDataset, RemixedFixedAudioDataset, RemixedRandomAudioDataset
 from gyraudio.default_locations import RAW_AUDIO_ROOT, MIXED_AUDIO_ROOT
 import logging
+import torch
 
 
 def test_single_audio():
@@ -54,3 +55,57 @@ def test_mixed_audio_trim():
     total_expected = len(get_dataloader({"full": get_config_dataloader(audio_root=MIXED_AUDIO_ROOT)})["full"].dataset)
 
     assert total_expected == 5000
+
+
+def test_remixed_audio():
+    total_size = 0
+    dl_train = get_dataloader(
+        {
+            TRAIN: get_config_dataloader(
+                audio_root=MIXED_AUDIO_ROOT,
+                mode=TRAIN,
+            )
+        },
+        audio_dataset=RemixedRandomAudioDataset
+    )[TRAIN]
+    dl_test = get_dataloader(
+        {
+            TEST: get_config_dataloader(
+                audio_root=MIXED_AUDIO_ROOT,
+                mode=TEST,
+            )
+        },
+        audio_dataset=RemixedFixedAudioDataset
+    )[TEST]
+    dataloaders = {
+        TRAIN: dl_train,
+        TEST: dl_test
+    }
+    batch_mix, batch_signal, batch_noise = next(iter(dataloaders[TRAIN]))
+    assert batch_mix.shape == batch_signal.shape == batch_noise.shape
+    train_size = len(dataloaders[TRAIN].dataset)
+    test_size = len(dataloaders[TEST].dataset)
+    total_size += train_size
+    print(f"TRAIN SIZE {train_size} TEST SIZE {test_size}")
+    total_expected = len(get_dataloader({"full": get_config_dataloader(audio_root=MIXED_AUDIO_ROOT)})["full"].dataset)
+    # No filter
+    assert total_size == total_expected == 5000
+
+    # Test 2 : fixed snr and noise
+    fixed_dataset_1 = RemixedFixedAudioDataset(MIXED_AUDIO_ROOT/"test")
+    fixed_dataset_2 = RemixedFixedAudioDataset(MIXED_AUDIO_ROOT/"test")
+    mixed_audio_signal, clean_audio_signal, noise_audio_signal = fixed_dataset_1[0]
+    mixed_audio_signal_2, clean_audio_signal_2, noise_audio_signal_2 = fixed_dataset_2[0]
+    fixed_snr = torch.norm(clean_audio_signal) / torch.norm(noise_audio_signal)
+    assert torch.equal(fixed_snr, torch.norm(clean_audio_signal_2) / torch.norm(noise_audio_signal_2))
+    assert torch.equal(noise_audio_signal, noise_audio_signal_2)
+
+    # Test 3 : random snr and noise
+    rnd_dataset_1 = RemixedRandomAudioDataset(MIXED_AUDIO_ROOT/"test")
+    rnd_dataset_2 = RemixedRandomAudioDataset(MIXED_AUDIO_ROOT/"test")
+    mixed_audio_signal, clean_audio_signal, noise_audio_signal = rnd_dataset_1[0]
+    mixed_audio_signal_2, clean_audio_signal_2, noise_audio_signal_2 = rnd_dataset_2[0]
+    assert torch.norm(clean_audio_signal) / torch.norm(noise_audio_signal) != torch.norm(clean_audio_signal_2) / torch.norm(noise_audio_signal_2)
+    assert fixed_snr != torch.norm(clean_audio_signal) / torch.norm(noise_audio_signal)
+    assert not(torch.equal(noise_audio_signal, noise_audio_signal_2))
+    assert torch.equal(clean_audio_signal, clean_audio_signal_2)
