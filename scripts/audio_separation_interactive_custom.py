1aaa290640c4ed99e2c765cc30cdf236cd5d4351
Balthazar Neveu <balthazarneveu@gmail.com>
2024-01-14 19:28:53 +0100

    #21 add audio_separation_interactive_custom to play with different audio tracks

---
+++ scripts/audio_separation_interactive_custom.py
@@ -0,0 +1,5 @@
+from gyraudio.audio_separation.visualization.interactive_audio import main_custom as interactive_audio_main_custom
+import sys
+
+if __name__ == "__main__":
+    interactive_audio_main_custom(sys.argv[1:])
