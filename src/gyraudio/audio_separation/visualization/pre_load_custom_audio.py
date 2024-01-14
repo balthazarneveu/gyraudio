95a7b4d56f73027ed16916eca0a3079778b6652e
Balthazar Neveu <balthazarneveu@gmail.com>
2024-01-14 16:40:16 +0100

    #21 preload generic audio files

---
+++ src/gyraudio/audio_separation/visualization/pre_load_custom_audio.py
@@ -0,0 +1,57 @@
+from batch_processing import Batch
+import argparse
+import sys
+from pathlib import Path
+from gyraudio.audio_separation.properties import PATHS, BUFFERS, NAME, GENERIC
+from gyraudio.io.audio import load_audio_tensor
+
+
+def parse_command_line_generic_audio_load() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser(description='Batch audio loading',
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument("-preload", "--preload", action="store_true", help="Preload audio files")
+    return parser
+
+
+def load_buffers(signal: dict, device="cpu", key=GENERIC) -> None:
+    generic_signal, sampling_rate = load_audio_tensor(signal[PATHS][key], device=device)
+    signal[BUFFERS] = {
+        key: generic_signal,
+    }
+    signal["sampling_rate"] = sampling_rate
+
+
+def audio_loading(input: Path, preload: bool, key=GENERIC) -> dict:
+    name = input.name
+    signal = {
+        NAME: name,
+        PATHS: {
+            key: input,
+        }
+    }
+    if preload:
+        load_buffers(signal, key=key)
+    return signal
+
+
+def generic_audio_loading_batch(input: Path, args: argparse.Namespace) -> dict:
+    """Wrapper to load audio files from a directory using batch_processing
+    """
+    return audio_loading(input, preload=args.preload)
+
+
+def main(argv):
+    batch = Batch(argv)
+    batch.set_io_description(
+        input_help='input audio files',
+        output_help=argparse.SUPPRESS
+    )
+    parser = parse_command_line_generic_audio_load()
+    batch.parse_args(parser)
+    all_signals = batch.run(generic_audio_loading_batch)
+    return all_signals
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
