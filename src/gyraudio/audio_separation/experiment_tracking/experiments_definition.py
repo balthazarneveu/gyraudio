2807c0ccb1627d8399bd431bcfe691e63c0bf78b
MathildeDupouy <82039222+MathildeDupouy@users.noreply.github.com>
2024-01-25 10:24:35 +0100

    #report Testing with another SNR range (1014)

--- src/gyraudio/audio_separation/experiment_tracking/experiments_definition.py
+++ src/gyraudio/audio_separation/experiment_tracking/experiments_definition.py
@@ -183,6 +183,14 @@ def exp_1004_waveunet(config, model: bool = None, minor=None):
     # 7 layers, ext +28 - Nvidia RTX3060 6Gb RAM - 16 batch size
     return config, model
 
+@registered_experiment(major=1004)
+def exp_1014_waveunet(config, model: bool = None, minor=None):
+    #trained with min and max mixing snr hard coded between -2 and -1
+    config[EPOCHS] = 50
+    config, model = exp_wave_unet(config, model=model, num_layers=7, channels_extension=28)
+    # 7 layers, ext +28 - Nvidia RTX3060 6Gb RAM - 16 batch size
+    return config, model
+
 
 def get_experiment_generator(exp_major: int):
     assert exp_major in REGISTERED_EXPERIMENTS_LIST, f"Experiment {exp_major} not registered"
