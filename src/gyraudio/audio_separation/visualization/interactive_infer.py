1929e27427f260710f1cc8991d294fffa02dffd4
MathildeDupouy <82039222+MathildeDupouy@users.noreply.github.com>
2024-01-27 00:10:52 +0100

    #11 enhance main + visu with regression line

--- src/gyraudio/audio_separation/visualization/interactive_infer.py
+++ src/gyraudio/audio_separation/visualization/interactive_infer.py
@@ -1,39 +1,71 @@
 from gyraudio.default_locations import EXPERIMENT_STORAGE_ROOT
 from gyraudio.audio_separation.parser import shared_parser
-from gyraudio.audio_separation.infer import launch_infer, already_inferred, DEFAULT_RECORD_FILE, DEFAULT_EVALUATION_FILE,  RECORD_KEYS, NBATCH, SNR_OUT, SNR_IN
+from gyraudio.audio_separation.infer import launch_infer, RECORD_KEYS, SNR_OUT, SNR_IN, NBATCH
 from gyraudio.audio_separation.properties import TEST, NAME, SHORT_NAME, CURRENT_EPOCH, SNR_FILTER 
-from gyraudio.audio_separation.experiment_tracking.storage import get_output_folder, load_checkpoint
-from gyraudio.audio_separation.experiment_tracking.experiments import get_experience
 import sys
+import os
 from dash import Dash, html, dcc, callback, Output, Input, dash_table
 import plotly.express as px
 import plotly.graph_objects as go
 import pandas as pd
+from typing import List
 import torch
 from pathlib import Path
 
 
 
-def get_app(eval_df : pd.DataFrame) :
-    eval_df = eval_df.sort_values(by=SNR_IN)
-
+def get_app(record_row_dfs : pd.DataFrame, eval_dfs : List[pd.DataFrame]) :
     app = Dash(__name__)
-    fig = px.scatter(eval_df, x=SNR_IN, y=SNR_OUT, marginal_y="histogram")
-    fig.add_trace(
-        go.Scatter(
-            x=eval_df[SNR_IN],
-            y=eval_df[SNR_IN],
-            mode="lines",
-            line=go.scatter.Line(color="gray"),
-            )
-    )
+    names = [f"{record[SHORT_NAME]} - {record[NAME]} epoch {record[CURRENT_EPOCH]:04d}" for idx, record in record_row_dfs.iterrows()]
     app.layout = html.Div([
-        html.H1(children='Visualize validation', style={'textAlign':'center'}),
-        dcc.Graph(id='graph-content', figure=fig),
-        # dash_table.DataTable(data=eval_df.sort_values(by=save_idx).to_dict('records'), page_size=10),
+        html.H1(children='Inference results', style={'textAlign':'center'}),
+        dcc.Dropdown(names, names[0], id='dropdown-selection'),
+        dcc.Graph(id='graph-content')
     ])
+    # eval_df = eval_df.sort_values(by=SNR_IN)
+    # app = Dash(__name__)
+    # fig = px.scatter(eval_df, x=SNR_IN, y=SNR_OUT, marginal_y="histogram")
+    # fig.add_trace(
+    #     go.Scatter(
+    #         x=eval_df[SNR_IN],
+    #         y=eval_df[SNR_IN],
+    #         mode="lines",
+    #         line=go.scatter.Line(color="gray"),
+    #         )
+    # )
+    # app.layout = html.Div([
+    #     html.H1(children='Visualize validation', style={'textAlign':'center'}),
+    #     dcc.Graph(id='graph-content', figure=fig),
+    #     # dash_table.DataTable(data=eval_df.sort_values(by=save_idx).to_dict('records'), page_size=10),
+    # ])
+
+    @callback(
+        Output('graph-content', 'figure'),
+        Input('dropdown-selection', 'value')
+    )
+    def update_graph(value):
+        id = (record_row_dfs.name == value).idxmax()
+        record = record_row_dfs.loc[id]
+        eval_df = eval_dfs[id].sort_values(by=SNR_IN)
+        title = f"SNR performance for experience {record[NAME]} in epoch {record[CURRENT_EPOCH]:04d}"
+
+        fig = px.scatter(eval_df, x=SNR_IN, y=SNR_OUT, title=title, marginal_y = "histogram", trendline='ols', trendline_color_override='darkblue')
+        fig.data[0].name = "Inference data"
+        fig.data[0].showlegend = True
+        fig.data[1].name = "Least Square regression"
+        fig.data[1].showlegend = True
+        # fig.add_trace(
+        #     go.Scatter(
+        #         x=eval_df[SNR_IN],
+        #         y=eval_df[SNR_IN],
+        #         mode="lines",
+        #         line=go.scatter.Line(color="gray"),
+        #         )
+        # ) 
+        return fig
     
     return app
+
     
 def main(argv):
     default_device = "cuda" if torch.cuda.is_available() else "cpu"
@@ -48,45 +80,22 @@ def main(argv):
     parser_def.add_argument("-s",  "--snr-filter", type=float, nargs="+", default=None,
                     help="SNR filters on the inference dataloader")
     args = parser_def.parse_args(argv)
+    record_row_dfs = pd.DataFrame(columns = RECORD_KEYS)
+    eval_dfs = []
     for exp in args.experiments:
-        # load experience
-        if args.snr_filter is not None :
-            args.snr_filter = sorted(args.snr_filter)
-        short_name, model, config, dl = get_experience(exp, snr_filter_test=args.snr_filter)
-        exists, exp_dir = get_output_folder(config, root_dir=Path(args.input_dir), override=False)
-        assert exp_dir.exists(), f"Experiment {short_name} does not exist in {Path(args.input_dir)}"
-        model.eval()
-        model.to(args.device)
-        model, optimizer, epoch, config_checkpt = load_checkpoint(model, exp_dir, epoch=None, device=args.device)
-        if args.output_dir is not None :
-            record_path = Path(args.output_dir)/DEFAULT_RECORD_FILE
-            assert record_path.exists()
-            record_df = pd.read_csv(record_path)
-            record_row = pd.DataFrame({
-                NAME: config[NAME],
-                SHORT_NAME: config[SHORT_NAME],
-                CURRENT_EPOCH: epoch,
-                NBATCH: args.nb_batch,
-                SNR_FILTER: [None],
-            }, index = [0], columns = RECORD_KEYS)
-            record_row.at[0, SNR_FILTER] = args.snr_filter
-            if not(already_inferred(record_df, record_row)) :
-                launch_infer(
-                    exp,
-                    model_dir=Path(args.input_dir),
-                    output_dir=Path(args.output_dir),
-                    device=args.device,
-                    force_reload=args.reload,
-                    max_batches=args.nb_batch,
-                    snr_filter=args.snr_filter
-                )
-            save_dir = Path(args.output_dir)/(exp_dir.name+"_infer"+ (f"_epoch_{epoch:04d}_nbatch_{args.nb_batch if args.nb_batch is not None else len(dl[TEST])}")
-                        + ("" if args.snr_filter is None else f"_snrs_{'_'.join(map(str, args.snr_filter))}"))
-            evaluation_path = save_dir/DEFAULT_EVALUATION_FILE
-            eval_df = pd.read_csv(evaluation_path)
-
-            app = get_app(eval_df)
-            app.run(debug=True)
+        record_row_df, evaluation_path = launch_infer(
+                exp,
+                model_dir=Path(args.input_dir),
+                output_dir=Path(args.output_dir),
+                device=args.device,
+                max_batches=args.nb_batch,
+                snr_filter=args.snr_filter
+            )
+        eval_df = pd.read_csv(evaluation_path)
+        record_row_dfs = pd.concat([record_row_df, record_row_dfs.loc[:]], ignore_index=True)
+        eval_dfs.append(eval_df)
+    app = get_app(record_row_dfs, eval_dfs)
+    app.run(debug=True)
 
 
 if __name__ == '__main__':
